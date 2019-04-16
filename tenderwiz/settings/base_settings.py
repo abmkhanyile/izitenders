1c94429e06215691fe37a495f6db722f4f8b76c5
Ayanda Khanyile <ayatech.co@gmail.com>
2019-04-16 12:44:28 +0200

    changes

--- tenderwiz/settings/base_settings.py
+++ tenderwiz/settings/base_settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,3 +144,27 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, '../static'),)
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/Media')
+
+
+GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
+
+
+import django_heroku
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
+
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('STORAGE_BUCKET_NAME', 'leadshub-staticfiles')
+AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID', 'AKIAIYBOZKG7LNFWN5FA')
+AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY', 'UX2iwXjKirMAQ+uDbhquu2yjZYhPFgHEpO/rKK90')
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
