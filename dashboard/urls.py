127793e42cff8940bc40823747e420a77f08e6a8
ayanda83 <abm.khanyile@gmail.com>
2019-08-01 23:19:23 +0200

    changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from .views import dashboard_view
 from tender_details.views import tenders_list_view
-from user_accounts.views import profile_view
+from user_accounts.views import profile_view, Invoice_view
 from .views import tenderList_view, tender_view
 
 urlpatterns = [
     url(r'^$', dashboard_view, name='dashboard'),
     url(r'^matched_tenders_list/(?P<date>\d\d-\d\d-\d\d\d\d)/$', tenderList_view, name='tender_list'),
     url(r'^tender/(?P<tender_pk>\d+)/$', tender_view, name='tender'),
+    url(r'^invoice/(?P<inv_id>\d+)/$', Invoice_view, name='dashboard_invoice'),
 ]
