46cf7cda88feb227ab1db4ba4e37e678aa983cc2
ayanda83 <abm.khanyile@gmail.com>
2020-02-04 14:56:25 +0200

    changes

--- tenderwiz/settings/development.py
+++ tenderwiz/settings/development.py
@@ -2,4 +2,4 @@ from .base_settings import *
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'j0fyf510lvqlmj&4fhhhcl=9=(c3f6_&n3pt0h!sso6f4@h5k#')
 
-ALLOWED_HOSTS = ['tenderwiz.herokuapp.com', 'www.leadshub.co.za', '.localhost', 'localhost:5000']
+ALLOWED_HOSTS = ['tradeworld.herokuapp.com', 'www.tradeworld.co.za', '.localhost', 'localhost:5000']
