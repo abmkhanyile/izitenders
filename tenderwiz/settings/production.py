8573e5de5eb5604e4caffd3ce3c6b33c8884cae9
Ayanda <ayatech.projects@gmail.com>
2022-04-26 17:10:23 +0200

    new changes

--- tenderwiz/settings/production.py
+++ tenderwiz/settings/production.py
@@ -1,6 +1,6 @@
 from .base_settings import *
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'j0fyf510lvqlmj&4fhhhcl=9=(c3f6_&n3pt0h!sso6f4@h5k#')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
